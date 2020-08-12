e2ba4b5d0bde72d4f947ae68edb901fa7551f547
Enrique Barboza <enriquebarbozajeb@gmail.com>
2020-08-11 21:34:15 -0700

    v3.0.0 - Now the library only applies the prefix that you browser needs (for example, before if you were in Opera, you needed the -o- prefix, but the library used '','-moz-','-webkit-' and '-o-' and now only use '-o-').
    - Double color attribute detection improved.
    
    - Now I have my own gradients in gradients.json, and you can use them using the jebg-grad
    attribute.
    
    - For random gradients, before you need to write '*random*' or leave the attribute without value,
    now, you must write '$random$' to get a random gradient.
    
    - 3 new console.error, one when you type an jebGradient that doesn't exist, one more when you
    can't fetch the uiGradients (maybe you don't have internet connection) and the last one when
    you cant fetch the jebGradients.

--- jebgradients.js
+++ jebgradients.js
@@ -1,6 +1,7 @@
 /* En este objeto se guardan los estilos de los mensajes y los links de los degradados */
 const jebgradients_const = {
     link: 'https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json',
+    link_j: 'https://cdn.jsdelivr.net/gh/jebbarbas/jebgradients/gradients.json',
     cssSuccess: 'color:lime;font-weight:bold;font-size:1rem;display:block',
     cssSuccessText: 'color:lightgreen;display:block',
     cssError: 'color:red;font-weight:bold;font-size:1rem;display:block',
@@ -45,19 +46,19 @@ class Gradient{
         else if(type === 'preradial'){
             /* Aqui se hace un switch en base al estilo predefinido */
             switch(direction){
-                case 'top-left': return 'ellipse farthest-corner at 0% 0%';
-                case 'top-center': return 'ellipse farthest-corner at 50% 0%';
-                case 'top-right': return 'ellipse farthest-corner at 100% 0%';
+                case 'top-left': return 'at 0% 0%';
+                case 'top-center': return 'at 50% 0%';
+                case 'top-right': return 'at 100% 0%';
 
-                case 'center-left': return 'ellipse farthest-corner at 0% 50%';
-                case 'center-center': return 'ellipse farthest-corner at 50% 50%';
-                case 'center-right': return 'ellipse farthest-corner at 100% 50%';
+                case 'center-left': return 'at 0% 50%';
+                case 'center-center': return 'at 50% 50%';
+                case 'center-right': return 'at 100% 50%';
 
-                case 'bottom-left': return 'ellipse farthest-corner at 0% 100%';
-                case 'bottom-center': return 'ellipse farthest-corner at 50% 100%';
-                case 'bottom-right': return 'ellipse farthest-corner at 100% 100%'; 
+                case 'bottom-left': return 'at 0% 100%';
+                case 'bottom-center': return 'at 50% 100%';
+                case 'bottom-right': return 'at 100% 100%'; 
 
-                default: return 'ellipse farthest-corner at 0% 0%';
+                default: return 'at 0% 0%';
             }
         }
     };
@@ -82,89 +83,145 @@ class Gradient{
         else {return ''};
     };
 
-    /* En base al tipo regresa una cadena diferente */
+    /* En base al tipo regresa un array de strings diferente */
     gradient = () => {
-        let gradientString = '';
+        let gradientArray = [];
 
         if(this.type === 'linear'){
             /* Se hace un estilo para cada navegador de acuerdo a this.prefixes */
             this.prefixes.forEach(prefix => {
-                gradientString += `background:${prefix}${this.repeat}linear-gradient(${this.direction},${this.colors}) !important;`;
+                gradientArray.push(`${prefix}${this.repeat}linear-gradient(${this.direction},${this.colors})`);
             });
         }
         else if(this.type === 'radial' || this.type === 'preradial'){
             this.prefixes.forEach(prefix => {
-                gradientString += `background:${prefix}${this.repeat}radial-gradient(${this.direction},${this.colors}) !important;`;
+                gradientArray.push(`${prefix}${this.repeat}radial-gradient(${this.direction},${this.colors})`);
             });
         }            
-
-        return gradientString;
+        return gradientArray;
     };
 
-    /* Verifica el viejo estilo del elemento */
-    static getStyle = (elemento = new Element()) => {
-        let style = elemento.getAttribute('style');
-        /* Si existe, verifica que termine con ';' para que no haya errores.
-        Si no existe, devuelve un string vacio para no devolver null */
-        if(style){
-            let arrayStyle = style.split('');
-            
-            if(arrayStyle[arrayStyle.length - 1] != ';'){
-                arrayStyle.push(';');
-            }
+    /* Funcion para aplicar el degradado a un elemento */
+    static apply = (element,objGradient = new Gradient()) => {
+        /* Se crea un array con todos los posibles estilos en base a los prefijos */
+        const backgroundArray = objGradient.gradient();
+        /* Para cada prefijo se le asigna al background del elemento
+        si la propiedad es invalida, no se aplica, BTW: solo se aplica 1 */
+        backgroundArray.forEach(background => {
+            element.style.background = background;
+        });
+    };
 
-            return arrayStyle.join('');
-        }
-        else{
-            return '';
-        }
+    /* Sirve para obtener el background de un elemento en los logs de random */
+    static getBackground = element => {
+        return `background:${element.style.background}`;
     };
 }
 /* Funcion que aplica los degradados a cada elemento con la clase jebg
 Tambien usa el JSON de ui-gradients para aplicar un degradado de estos */
 const jebgradients_apply = () => {
+    /* Funcion que busca entre un element todos los atributos de color, excepto uno(only)
+    devuelve true si no existen los demas y false si se encuentran */
     let totalErrors = 0;
-    const allElementsWithGradients = document.querySelectorAll('*[jebg-colors]');
+    const isOnlyIn = (only = '',element) => {
+        if(only == 'jebg-ui'){
+            if(!element.getAttribute('jebg-colors') && !element.getAttribute('jebg-grad')){
+                return true;
+            }
+            return false;
+        }
+        if(only == 'jebg-colors'){
+            if(!element.getAttribute('jebg-ui') && !element.getAttribute('jebg-grad')){
+                return true;
+            }
+            return false;
+        }
+        if(only == 'jebg-grad'){
+            if(!element.getAttribute('jebg-colors') && !element.getAttribute('jebg-ui')){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    };
+    const allElementsWithGradients = document.querySelectorAll('*[jebg-colors],*[jebg-grad],*[jebg-ui]');
     allElementsWithGradients.forEach(htmlEl => {
-        /* Verifica si no contiene el otro atributo de color, si lo tiene marca error */
-        if(!htmlEl.getAttribute('jebg-ui')){
+        /* Verifica que atributo de color tiene */
+        if(isOnlyIn('jebg-colors',htmlEl)){
+            /* Se guardan los valores de los atributos del elemento */
             const j_type = htmlEl.getAttribute('jebg-type') || 'linear';
             const j_dir = htmlEl.getAttribute('jebg-dir') || 'right';
             const j_colors = htmlEl.getAttribute('jebg-colors') || 'transparent';
             const j_repeat = (htmlEl.getAttribute('jebg-repeat')) ? true : false;
 
-            const deg = new Gradient(j_type,j_dir,j_colors,j_repeat);
+            const gradient = new Gradient(j_type,j_dir,j_colors,j_repeat);
 
-            htmlEl.setAttribute('style',Gradient.getStyle(htmlEl)+deg.gradient());
+            Gradient.apply(htmlEl,gradient);
         }
-        else{
-            totalErrors++;
-            console.error(`%cError❌ %cThis element contains both jebg-colors and jebg-ui, please write only one of these in %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
-        }
-    });
-
-    const allElementsWithuiGradients = document.querySelectorAll('*[jebg-ui]');
-    allElementsWithuiGradients.forEach(htmlEl => {
-        /* Verifica si no contiene el otro atributo de color, si lo tiene marca error */
-        if(!htmlEl.getAttribute('jebg-colors')){
+        else if(isOnlyIn('jebg-grad',htmlEl)){
             const j_type = htmlEl.getAttribute('jebg-type') || 'linear';
             const j_dir = htmlEl.getAttribute('jebg-dir') || 'right';
-            const j_uiName = htmlEl.getAttribute('jebg-ui').trim() || '*random*';
+            const j_jebName = htmlEl.getAttribute('jebg-grad').trim() || 'null';
             const j_repeat = (htmlEl.getAttribute('jebg-repeat')) ? true : false;
 
             /* Verifica si se ha especificado un nombre, sino, aplica un degradado random */
-            if(j_uiName !== '*random*'){
+            if(j_jebName !== '$random$'){
+                fetch(jebgradients_const.link_j).then(result => result.json()).then(jebGradients => {
+                    let jebGradient = jebGradients.find(gradient => gradient.name == j_jebName);
+                    if(jebGradient){
+                        let jebColors = jebGradient.colors.join(); 
+                        const gradient = new Gradient(j_type,j_dir,jebColors,j_repeat);
+
+                        Gradient.apply(htmlEl,gradient);
+                    }
+                    else{
+                        totalErrors++;
+                        console.error(`%cError❌ %cCannot find the jebGradient named: "${j_jebName}" in %c${htmlEl.outerHTML}. %cAre you sure that jebGradient exists and its well written?`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
+                    }
+                }).catch(err => {
+                    totalErrors++;
+                    console.error(`%cError❌ %c${err}, are you connected to internet and you can connect to ${jebgradients_const.link_j}? %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
+                });
+            }
+            else{
+                fetch(jebgradients_const.link_j).then(result => result.json()).then(jebGradients => {
+                    let randomNumber = Math.floor(Math.random() * jebGradients.length );
+                    let randomGradient = jebGradients[randomNumber];
+
+                    let jebColors = randomGradient.colors.join(); 
+                    const gradient = new Gradient(j_type,j_dir,jebColors,j_repeat);
+
+                    Gradient.apply(htmlEl,gradient);
+                    console.log(`%c${randomGradient.name}`,'padding:.5rem;'+Gradient.getBackground(htmlEl));
+                }).catch(err => {
+                    totalErrors++;
+                    console.error(`%cError❌ %c${err}, are you connected to internet and you can connect to ${jebgradients_const.link_j}? %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
+                });
+            }
+        }
+        else if(isOnlyIn('jebg-ui',htmlEl)){
+            const j_type = htmlEl.getAttribute('jebg-type') || 'linear';
+            const j_dir = htmlEl.getAttribute('jebg-dir') || 'right';
+            const j_uiName = htmlEl.getAttribute('jebg-ui').trim() || 'null';
+            const j_repeat = (htmlEl.getAttribute('jebg-repeat')) ? true : false;
+
+            /* Verifica si se ha especificado un nombre, sino, aplica un degradado random */
+            if(j_uiName !== '$random$'){
                 fetch(jebgradients_const.link).then(result => result.json()).then(uiGradients => {
                     let uiGradient = uiGradients.find(gradient => gradient.name == j_uiName);
                     if(uiGradient){
                         let uiColors = uiGradient.colors.join(); 
-                        const deg = new Gradient(j_type,j_dir,uiColors,j_repeat);
-                        htmlEl.setAttribute('style',Gradient.getStyle(htmlEl)+deg.gradient());
+                        const gradient = new Gradient(j_type,j_dir,uiColors,j_repeat);
+
+                        Gradient.apply(htmlEl,gradient);
                     }
                     else{
                         totalErrors++;
                         console.error(`%cError❌ %cCannot find the uiGradient named: "${j_uiName}" in %c${htmlEl.outerHTML}. %cAre you sure that uiGradient exists and its well written?`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
                     }
+                }).catch(err => {
+                    totalErrors++;
+                    console.error(`%cError❌ %c${err}, are you connected to internet and you can connect to ${jebgradients_const.link}? %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
                 });
             }
             else{
@@ -173,18 +230,23 @@ const jebgradients_apply = () => {
                     let randomGradient = uiGradients[randomNumber];
 
                     let uiColors = randomGradient.colors.join(); 
-                    const deg = new Gradient(j_type,j_dir,uiColors,j_repeat);
+                    const gradient = new Gradient(j_type,j_dir,uiColors,j_repeat);
 
-                    console.log(`%c${randomGradient.name}`,'padding:.5rem;'+deg.gradient());
-                    htmlEl.setAttribute('style',Gradient.getStyle(htmlEl)+deg.gradient());
+                    Gradient.apply(htmlEl,gradient);
+
+                    console.log(`%c${randomGradient.name}`,'padding:.5rem;'+Gradient.getBackground(htmlEl));
+                }).catch(err => {
+                    totalErrors++;
+                    console.error(`%cError❌ %c${err}, are you connected to internet and you can connect to ${jebgradients_const.link}? %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
                 });
             }
         }
         else{
             totalErrors++;
-            console.error(`%cError❌ %cThis element contains both jebg-colors and jebg-ui, please write only one of these in %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
+            console.error(`%cError❌ %cThis element contains two or more of this color-attributes: jebg-colors, jebg-ui or jebg-grad. Please write only one of these in %c${htmlEl.outerHTML}`,jebgradients_const.cssError,jebgradients_const.cssErrorText,jebgradients_const.cssErrorText);
         }
     });
+
     /* Mensaje para avisar que los degradados han sido aplicados o ha habido errores */
     if(totalErrors == 0){
         console.log(`%cJebGradients Applied✔️ %cUse jebgradients_apply(); to recharge them. See the repo and documentation in: 'https://github.com/JebBarbas/jebgradients' `,jebgradients_const.cssSuccess,jebgradients_const.cssSuccessText);
